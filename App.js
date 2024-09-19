917d3f0a77eb99aa68b6e938555bd0820fde73ad
unknown <guidostoccodev@gmail.com>
2024-09-19 20:26:23 -0300

    Criando a lógica de selecionar uma moeda

--- App.js
+++ App.js
@@ -1,14 +1,53 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { useEffect, useState } from 'react';
 import {PickerItem} from './src/Picker'
+import {api} from './src/services/api'
+
 
 export default function App() {
+  const [loading, setLoading] = useState(true)
+  const [moedas, setMoedas] = useState([])
+  const [moedaSelecionada, setMoedaSelecionada] = useState(null)
+
+  useEffect(() => {
+    async function loadMoedas(){
+      const response = await api.get("all")
+      let arrayMoedas = []
+
+      Object.keys(response.data).map((key) => {
+        arrayMoedas.push({
+          key: key,
+          value: key,
+          label: key
+        })
+      })
+
+      setMoedas(arrayMoedas);
+      setMoedaSelecionada(arrayMoedas[0].key)
+      setLoading(false);
+    }
+
+    loadMoedas();
+  }, [])
+
+  if(loading){
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#101215'}}>
+        <ActivityIndicator color='#fff' size={35}/>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
         <View style={styles.areaMoeda}>
           <Text style={styles.titulo}>Selecione</Text>
-          <PickerItem/>
+          <PickerItem
+          moedas={moedas}
+          moedaSelecionada={moedaSelecionada}
+          onChange={(moeda) => setMoedaSelecionada(moeda)}
+          />
         </View>
     </View>
   );
